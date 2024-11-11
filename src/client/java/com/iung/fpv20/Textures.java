7d721fac38bb41674f1987f878beadf46ac0b4d0
wefcdse <3371348169@qq.com>
2024-11-11 12:42:25 +0800

    disable this branch
    Signed-off-by: wefcdse <3371348169@qq.com>

--- src/client/java/com/iung/fpv20/Textures.java
+++
@@ -1,8 +0,0 @@
-package com.iung.fpv20;
-
-import net.minecraft.util.Identifier;
-
-public class Textures {
-    public static final Identifier COLOURED =  Identifier.of(Fpv20.MOD_ID,
-            "textures/utils/coloured.png");
-}
