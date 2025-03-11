a4bda449d6cfc119d94accf113ccb8c92ddf71c6
wefcdse <3371348169@qq.com>
2025-03-11 23:16:12 +0800

    update start flying

--- src/client/java/com/iung/fpv20/flying/GlobalFlying.java
+++ src/client/java/com/iung/fpv20/flying/GlobalFlying.java
@@ -200,8 +200,8 @@ public class GlobalFlying {
         if (this.last_tick_flying != getFlying()) {
             float yaw = p.getYaw();
             float pitch = p.getPitch();
-            drone.re_init();
-            drone.update_pose(PhysicsCore.from_ypr_deg(yaw, pitch, 0));
+            drone.re_init(p);
+//            drone.update_pose(PhysicsCore.from_ypr_deg(yaw, pitch- config1.getCamera_angle(), 0));
         }
 
         Quaternionf q = drone.get_pose();
@@ -442,7 +442,7 @@ public class GlobalFlying {
         if (this.last_tick_flying != getFlying()) {
             float yaw = p.getYaw();
             float pitch = p.getPitch();
-            drone.re_init();
+            drone.re_init(p);
             if (Fpv20Client.config1.free_camera_yaw) {
                 p.setYaw(180);
             }
@@ -452,7 +452,7 @@ public class GlobalFlying {
             }
             Fpv20.LOGGER.info("start flying");
 
-            drone.update_pose(PhysicsCore.from_ypr_deg(yaw, pitch, 0));
+//            drone.update_pose(PhysicsCore.from_ypr_deg(yaw, pitch, 0));
         }
         this.last_tick_flying = getFlying();
 
